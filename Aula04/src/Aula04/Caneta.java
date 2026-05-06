161fceb66d42c142d077533ff0212d814030fb31
Yasmim Campos <yzamimsilvacampos@gmail.com>
2026-05-05 21:19:22 -0300

    Add files via upload

---
+++ Aula04/src/Aula04/Caneta.java
@@ -0,0 +1,30 @@
+package Aula04;
+public class Caneta {
+    public String modelo;
+    private float ponta;
+
+    //get (pega o valor)
+    //setter(altera o valor)
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    public float getPonta() {
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p;
+
+
+
+
+
+
+
+    }
+
+
+}
