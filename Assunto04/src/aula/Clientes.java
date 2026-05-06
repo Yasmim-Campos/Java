f87516cdb839ddbbf7b2feb39b35c6822c137153
Yasmim Campos <yzamimsilvacampos@gmail.com>
2026-05-06 19:15:14 -0300

    Implement Clientes class with attributes and methods
    Added fields and methods for client data management.

--- Assunto04/src/aula/Clientes.java
+++ Assunto04/src/aula/Clientes.java
@@ -1,5 +1,31 @@
 package aula;
 
 public class Clientes {
-
-}
+	protected String nome,email;
+	protected String numero; 
+	
+	public String getNome() {return this.nome;}
+	
+	public void getEmail() {return this.email;}
+	
+	public int getNumero() {return this.numero;}
+	
+	
+	public void setNome(String nome) {this.nome = nome: }
+	public void setEmail(String nome) {this.email = email: }
+	public void setNumero(int num)  {this.numero = num} 
+	
+	
+	public void MostrarDados() {
+		System.out.println("DADOS CLIENTES FISICAS"); 
+		System.out.println("Nome" + this.nome );
+		System.out.println("Email" + this.email );
+		System.out.println("Numero" + this.numero );
+		
+		
+		
+	}
+	
+	
+	
+	
