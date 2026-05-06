4cd8dd2f7b432ca24bec6e432e49b8e8243f8579
Yasmim Campos <yzamimsilvacampos@gmail.com>
2026-05-06 19:15:40 -0300

    Extend clientesFisicos class with RG and CPF attributes
    Added attributes and methods for RG and CPF in clientesFisicos class.

--- Assunto04/src/aula/clientesFisicos.java
+++ Assunto04/src/aula/clientesFisicos.java
@@ -1,5 +1,24 @@
 package aula;
 
-public class clientesFisicos {
+public class clientesFisicos extends Clientes {
+	protected String rg,cpf;
 
 }
+
+public String get.RG () {return this.rg];
+public String get.CPF() {return this.cpf;}
+
+public void setRG(String RG) {this.rg = rg}; 
+public void setCPF(String Cpf) {this.cpf= cpf}
+
+public void MostrarDados() {
+	
+
+
+
+
+}
+}
+
+
+
